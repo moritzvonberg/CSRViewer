95a12f4ffc5389a15d3cb6841ae35143f6931663
Moritz von Berg <moritzvonberg@gmail.com>
2023-07-18 10:50:57 +0200

    added missing diffs from initial commit

--- src/test/java/com/vonberg/csrviewer/CertificateSigningRequestAssert.java
+++ src/test/java/com/vonberg/csrviewer/CertificateSigningRequestAssert.java
@@ -1,4 +1,8 @@
-package com.vonberg.CSRViewer;
+package com.vonberg.csrviewer;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.assertj.core.api.AbstractAssert;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
@@ -7,52 +11,49 @@ import org.bouncycastle.asn1.x509.GeneralName;
 import org.bouncycastle.operator.DefaultAlgorithmNameFinder;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class CertificateSigningRequestAssert extends AbstractAssert<CertificateSigningRequestAssert, PKCS10CertificationRequest> {
 
     static final DefaultAlgorithmNameFinder finder = new DefaultAlgorithmNameFinder();
+
     public CertificateSigningRequestAssert(PKCS10CertificationRequest actual) {
         super(actual, CertificateSigningRequestAssert.class);
     }
 
-    public static CertificateSigningRequestAssert assertThat(PKCS10CertificationRequest actual){
+    public static CertificateSigningRequestAssert assertThat(PKCS10CertificationRequest actual) {
         return new CertificateSigningRequestAssert(actual);
     }
 
-    public CertificateSigningRequestAssert subjectNameToStringEquals(String expected){
+    public CertificateSigningRequestAssert subjectNameToStringEquals(String expected) {
         isNotNull();
-        if (!actual.getSubject().toString().equals(expected)){
+        if (!actual.getSubject().toString().equals(expected)) {
             failWithMessage("Expected CSR subject name to be %s but got %s", expected, actual.getSubject());
         }
         return this;
     }
 
-    public CertificateSigningRequestAssert algorithmIdEquals(AlgorithmIdentifier id){
+    public CertificateSigningRequestAssert algorithmIdEquals(AlgorithmIdentifier id) {
         isNotNull();
-        if (!actual.getSignatureAlgorithm().equals(id)){
+        if (!actual.getSignatureAlgorithm().equals(id)) {
             failWithMessage("Expected CSR algorithm to be %s but got %s", finder.getAlgorithmName(id),
                     finder.getAlgorithmName(actual.getSignatureAlgorithm()));
         }
         return this;
     }
 
-    public CertificateSigningRequestAssert hasNoAltNames(){
+    public CertificateSigningRequestAssert hasNoAltNames() {
         isNotNull();
         var altNames = PKCS10Parser.getAltNames(actual);
-        if (altNames != null){
+        if (altNames != null) {
             failWithMessage("Expected CSR to have no alternative names, but it contained: %s",
                     Arrays.stream(altNames).map(GeneralName::toString).collect(Collectors.joining(", ")));
         }
         return this;
     }
 
-    public CertificateSigningRequestAssert altNamesContainParsedValue(String value){
+    public CertificateSigningRequestAssert altNamesContainParsedValue(String value) {
         isNotNull();
         var altNames = PKCS10Parser.getAltNames(actual);
-        if (altNames == null){
+        if (altNames == null) {
             failWithMessage("Expected CSR to contain alt name %s but it didn't contain any alt names.", value);
         } else if (Arrays.stream(altNames).noneMatch(x -> x.toString().equals(value))) {
             failWithMessage("Expected CSR to contain alt name %s, but it doesn't.", value);
@@ -60,33 +61,33 @@ public class CertificateSigningRequestAssert extends AbstractAssert<CertificateS
         return this;
     }
 
-    public CertificateSigningRequestAssert signatureIsValid(){
+    public CertificateSigningRequestAssert signatureIsValid() {
         return signatureValidationResultEquals(PKCS10Parser.ValidationResultState.VALID);
     }
 
-    public CertificateSigningRequestAssert signatureIsInvalid(){
+    public CertificateSigningRequestAssert signatureIsInvalid() {
         return signatureValidationResultDoesNotEqual(PKCS10Parser.ValidationResultState.VALID);
     }
 
-    public CertificateSigningRequestAssert signatureValidationResultEquals(PKCS10Parser.ValidationResultState state){
+    public CertificateSigningRequestAssert signatureValidationResultEquals(PKCS10Parser.ValidationResultState state) {
         isNotNull();
         var validationAttemptResult = PKCS10Parser.tryValidateSignature(actual);
-        if (!validationAttemptResult.equals(state)){
+        if (!validationAttemptResult.equals(state)) {
             failWithMessage("Expected VerificationResultState %s, but got state %s", state.name(), validationAttemptResult.name());
         }
         return this;
     }
 
-    public CertificateSigningRequestAssert signatureValidationResultDoesNotEqual(PKCS10Parser.ValidationResultState state){
+    public CertificateSigningRequestAssert signatureValidationResultDoesNotEqual(PKCS10Parser.ValidationResultState state) {
         isNotNull();
         var validationAttemptResult = PKCS10Parser.tryValidateSignature(actual);
-        if (validationAttemptResult.equals(state)){
+        if (validationAttemptResult.equals(state)) {
             failWithMessage("Expected VerificationResultState not to be %s, but it was", state.name());
         }
         return this;
     }
 
-    public CertificateSigningRequestAssert subjectNameContains(Map<ASN1ObjectIdentifier, String> keyValuePairs){
+    public CertificateSigningRequestAssert subjectNameContains(Map<ASN1ObjectIdentifier, String> keyValuePairs) {
         return this;
     }
 }
--- src/test/java/com/vonberg/csrviewer/TestCsrBuilder.java
+++ src/test/java/com/vonberg/csrviewer/TestCsrBuilder.java
@@ -1,4 +1,8 @@
-package com.vonberg.CSRViewer;
+package com.vonberg.csrviewer;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
 
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.Attribute;
@@ -8,12 +12,20 @@ import org.bouncycastle.pkcs.PKCS10CertificationRequest;
 import org.bouncycastle.pkcs.PKCS10CertificationRequestBuilder;
 import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.SecureRandom;
-
 
 public class TestCsrBuilder {
+
+    public static KeyPairAndCsrPair buildCSR(X500Name name, String keyAlgorithmId, String signatureAlgorithmId, int keySize, Attribute[] attributes) throws Exception {
+        var generator = KeyPairGenerator.getInstance(keyAlgorithmId);
+        generator.initialize(keySize, new SecureRandom());
+        KeyPair pair = generator.generateKeyPair();
+        PKCS10CertificationRequestBuilder builder = new JcaPKCS10CertificationRequestBuilder(name, pair.getPublic());
+        for (var attribute : attributes) {
+            builder.addAttribute(attribute.getAttrType(), attribute.getAttrValues());
+        }
+        ContentSigner signer = new JcaContentSignerBuilder(signatureAlgorithmId).build(pair.getPrivate());
+        return new KeyPairAndCsrPair(builder.build(signer), pair);
+    }
 
     record KeyPairAndCsrPair(PKCS10CertificationRequest request, KeyPair keyPair) {
         KeyPairAndCsrPair {
@@ -21,19 +33,6 @@ public class TestCsrBuilder {
                 throw new IllegalArgumentException("Null provided instead of request or keypair!");
             }
         }
-        }
-
-
-    public static KeyPairAndCsrPair buildCSR(X500Name name, String keyAlgorithmId, String signatureAlgorithmId, int keySize, Attribute[] attributes) throws Exception{
-        var generator = KeyPairGenerator.getInstance(keyAlgorithmId);
-        generator.initialize(keySize, new SecureRandom());
-        KeyPair pair = generator.generateKeyPair();
-        PKCS10CertificationRequestBuilder builder = new JcaPKCS10CertificationRequestBuilder(name, pair.getPublic());
-        for (var attribute: attributes){
-            builder.addAttribute(attribute.getAttrType(), attribute.getAttrValues());
-        }
-        ContentSigner signer = new JcaContentSignerBuilder(signatureAlgorithmId).build(pair.getPrivate());
-        return new KeyPairAndCsrPair(builder.build(signer), pair);
     }
 
 }
